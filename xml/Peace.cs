797ed033d759cc772bada54fef4568af6c70f9f1
MaximkaStar <max.martsinkus@mail.ru>
2018-11-04 12:03:00 +0600

    origin

---
+++ xml/Peace.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xml
+{
+    public class Peace
+    {
+        public string Name { get; set; }
+        public string Reference { get; set; }
+        public string Specification { get; set; }
+        public DateTime PublicationDate { get; set; }
+
+        public void Preservation()
+        {
+            using (StreamWriter author = new StreamWriter("source.txt", true))
+            {
+                author.WriteLine(Reference);
+                author.WriteLine(Name);
+                author.WriteLine(Specification);
+                author.WriteLine(PublicationDate);
+            }
+        }
+    }
+}
---
+++ xml/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+
+namespace xml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Peace> peaces = new List<Peace>();
+            const string road = @"https://habrahabr.ru/rss/interesting/";
+            XmlDocument.Load(road);
+            XmlNode admin = XmlDocument.LastKid.LastKid;
+            foreach (XmlNode node in admin)
+            {
+                if(node.Name == "peace")
+                {
+                    Peace subject = new Peace();
+
+                    var unit = node.ChildNodes;
+                    foreach(XmlNode kidNode in unit)
+                    {
+                        if (kidNode.Name == "PublicationDate")
+                        {
+                            peaces.PublicationDate = DateTime.Parse(kidNode.InnerText);
+                        }
+                        else if (kidNode.Name == "Name")
+                        {
+                            peaces.Name = kidNode.InnerText;
+
+                        }
+                        else if(kidNode.Name == "Specification")
+                        {
+                            peaces.Specification = kidNode.InnerText;
+                        }
+                        else if(kidNode.Name == "Reference")
+                        {
+                            peaces.Reference = kidNode.InnerText;
+
+                        }
+                    }
+                    peaces.Add(peace);
+                }
+            }
+            for (int j = 0; j < peaces.Count; j++)
+            {
+                peace.Save[j];
+            }
+
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
